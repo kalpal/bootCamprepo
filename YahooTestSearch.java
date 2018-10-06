89d66f39302892ff4a6045d494e63b48e4969fce
Bakhtiyor Makhsimov <bakhtiyor.makhsimov@gmail.com>
2018-10-06 14:44:44 -0700

    test commit

---
+++ YahooTestSearch.java
@@ -0,0 +1,35 @@
+package loginEx;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class YahooTestSearch {
+
+    private WebDriver driver;
+
+
+    @BeforeTest
+    public void setupDriver() {
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.OFF);
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Bakhtiyor\\IdeaProjects\\day_one_a\\src\\test\\resource\\geckodriver.exe");
+        driver = new FirefoxDriver();
+
+    }
+
+    @Test
+    public void test001() throws Exception {
+        driver.get("http://www.yahoo.com");
+    }
+
+    @AfterTest
+    public void closeBrowser(){
+        driver.quit();
+    }
+}
